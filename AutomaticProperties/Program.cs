e013124f0dccf1f458eaa7cf782f87b3ffeddf56
abenteuerzeit <amroz.muzyka@gmail.com>
2022-08-26 11:00:38 +0200

    Implement polygons

--- AutomaticProperties/Program.cs
+++ AutomaticProperties/Program.cs
@@ -6,7 +6,13 @@ namespace AutomaticProperties
     {
         static void doWork()
         {
-            // TODO:
+            Polygon square = new Polygon();
+            Polygon triangle = new Polygon { NumSides = 3 };
+            Polygon pentagon = new Polygon { SideLength = 15.5, NumSides = 5 };
+
+            Console.WriteLine($"Square: the number of sides is {square.NumSides}, the length of each side is {square.SideLength}");
+            Console.WriteLine($"Triangle: the number of sides is {triangle.NumSides}, the length of each side is {triangle.SideLength}");
+            Console.WriteLine($"Pentagon: the number of sides is {pentagon.NumSides}, the length of each side is {pentagon.SideLength}");
         }
 
         static void Main(string[] args)
