5a9305178f05ee6592b3414923f624702de94a7a
abenteuerzeit <amroz.muzyka@gmail.com>
2022-08-26 10:55:27 +0200

    Add polygon class

---
+++ AutomaticProperties/Polygon.cs
@@ -0,0 +1,14 @@
+﻿namespace AutomaticProperties
+{
+    internal class Polygon
+    {
+        public int NumSides { get; set; }
+        public double SideLength { get; set; }
+
+        public Polygon()
+        {
+            this.NumSides = 4;
+            this.SideLength = 10.0;
+        }
+    }
+}
